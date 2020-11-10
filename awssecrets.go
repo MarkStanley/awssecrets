1638d4ded6b966dcd421711277544ddddd50d01b
Mark Stanley <mark.stanley@coraledge.co.uk>
2020-11-10 12:07:02 +0000

    first commit

---
+++ awssecrets.go
@@ -0,0 +1,85 @@
+package awssecrets
+
+// Makes use of AWS boilerplate code
+// 2 functions: GetAllSecrets and GetSecret, and a global - AllSecrets
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+)
+
+// AllSecrets is the map of the AWS Secrets returned from getAllSecrets
+var AllSecrets map[string]interface{}
+
+// GetAllSecrets should be called at the start of your main() to populate the global AllSecrets
+func GetAllSecrets(secretsName string) (allSecrets map[string]interface{}) {
+	region := "eu-west-2"
+
+	//Create a Secrets Manager client
+	svc := secretsmanager.New(session.New(),
+		aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretsName),
+		VersionStage: aws.String("AWSCURRENT"),
+	}
+
+	// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+	// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeDecryptionFailure:
+				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+
+			case secretsmanager.ErrCodeInternalServiceError:
+				// An error occurred on the server side.
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidParameterException:
+				// You provided an invalid value for a parameter.
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidRequestException:
+				// You provided a parameter value that is not valid for the current state of the resource.
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				// We can't find the resource that you asked for.
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	// Decrypts secret using the associated KMS CMK.
+	// Depending on whether the secret is a string or binary, one of these fields will be populated.
+	var secretString string
+	if result.SecretString != nil {
+		secretString = *result.SecretString
+	}
+
+	// AWS Secrets Mgr gives us all the secrets in a json structured string of text.
+	// We need to unmarshall it into a map so we can extract the required values.
+
+	json.Unmarshal([]byte(secretString), &allSecrets)
+	return allSecrets
+}
+
+// GetSecret returns individual secrets out of AllSecrets as strings
+func GetSecret(varName string) (varValue string) {
+	varValue = fmt.Sprint(AllSecrets[varName])
+	//log.Println(varName+": ", varValue)
+	return varValue
+}
